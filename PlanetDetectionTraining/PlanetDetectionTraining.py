388865bbac664a7a29f196e7038084d568bdff5a
noya-isaiah <noya2110@gmail.com>
2024-05-25 17:49:38 +0300

    Add project files.

---
+++ PlanetDetectionTraining/PlanetDetectionTraining.py
@@ -0,0 +1,133 @@
+﻿#imports
+import pathlib
+import cv2 as cv
+import numpy as np
+
+import matplotlib.pyplot as plt
+import random
+
+import tensorflow as tf
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+
+from sklearn.metrics import confusion_matrix,classification_report,ConfusionMatrixDisplay
+from sklearn.preprocessing import LabelEncoder
+
+interactive_mode = False
+
+data_dir = pathlib.Path('./Datasets/Planets_Moons_Data/Planets and Moons') 
+image_files = list(data_dir.glob('**/*.jpg'))
+
+#picturs examples
+for i in range(10):
+  sample_img_path = str(random.choice(image_files))
+  planet_name = sample_img_path.split('/')[-1]
+  sample_img = cv.imread(sample_img_path)
+  if (interactive_mode == True) :
+    plt.imshow(cv.cvtColor(sample_img, cv.COLOR_BGR2RGB))
+    plt.axis("off")
+    plt.title(label = str(planet_name))
+    plt.show()
+
+def data_load(image_files):
+    X = []
+    Y = []
+
+    random.shuffle(image_files)
+
+    for file in image_files:
+        img = cv.imread(str(file))
+        planet_name = file.parent.name
+
+        X.append(img)
+        Y.append(str(planet_name))
+
+    X = np.array(X)
+
+    return X, Y
+
+loaded_data= data_load(image_files)
+
+def split_data(X, Y, split_percentage=[70, 20]):
+    train_size = int(split_percentage[0] / 100 * len(X))
+    val_size = int(split_percentage[1] / 100 * len(X))
+    test_size = len(X) - train_size - val_size
+
+    train_X, train_Y = X[:train_size], Y[:train_size]
+    valid_X, valid_Y = X[train_size:train_size + val_size], Y[train_size:train_size + val_size]
+    test_X, test_Y = X[train_size + val_size:], Y[train_size + val_size:]
+
+    return (train_X, train_Y), (valid_X, valid_Y), (test_X, test_Y)
+
+(train_X, train_Y), (valid_X, valid_Y), (test_X, test_Y) = split_data(loaded_data[0], loaded_data[1])
+
+print(len(train_X), "train")
+print(len(valid_X), "valid")
+print(len(test_X), "test")
+
+print(train_X.shape)
+
+train_images = train_X / 255.0
+test_images = test_X / 255.0
+valid_images = valid_X / 255.0
+
+label_encoder = LabelEncoder()
+train_labels_encoded = label_encoder.fit_transform(train_Y)
+test_labels_encoded = label_encoder.transform(test_Y)
+valid_labels_encoded = label_encoder.transform(valid_Y)
+
+print(label_encoder.classes_)
+
+model = Sequential([
+    Conv2D(32, kernel_size=3, activation='relu', input_shape=(144, 256, 3)),
+    MaxPooling2D(pool_size=2),
+    Conv2D(64, kernel_size=3, activation='relu'),
+    MaxPooling2D(pool_size=2),
+    Flatten(),
+    Dense(11, activation='softmax')
+])
+
+plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]
+)
+
+history = model.fit(train_images, train_labels_encoded, validation_data=(valid_images, valid_labels_encoded), epochs=4)
+
+loss = history.history['loss']
+accuracy = history.history['sparse_categorical_accuracy']
+val_loss = history.history['val_loss']
+val_accuracy = history.history['val_sparse_categorical_accuracy']
+
+if (interactive_mode == True) :
+    plt.plot(loss, 'g');
+    plt.plot(val_loss, 'r');
+    plt.title('Training loss',fontsize=20);
+
+    plt.plot(accuracy, 'b');
+    plt.plot(val_accuracy, 'r');
+    plt.title('Training accuracy',fontsize=20);
+
+y_pred = model.predict(test_images)
+predicted_labels = tf.argmax(y_pred, axis=1)
+true_labels = test_labels_encoded
+predicted_labels = predicted_labels.numpy()
+
+confusion_matrix = tf.math.confusion_matrix(true_labels, predicted_labels)
+confusion_matrix_np = confusion_matrix.numpy()
+
+
+if (interactive_mode == True) :
+    disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix_np)
+    disp.plot(cmap='Blues')
+    plt.title('Confusion Matrix')
+    plt.show()
+
+print(classification_report(true_labels, predicted_labels))
+
+model.save('../models/planet_detecion_model.keras', save_format='tf')
+
---
+++ PlanetDetrectionUI/PlanetDetrectionUI.py
@@ -0,0 +1,119 @@
+import numpy as np
+import sys
+import keras
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QFileDialog, QPushButton, QLabel
+from PyQt5.QtGui import QPixmap, QImage
+
+class Ui_MainWindow(QDialog):
+    
+    def setupUI(self, MainWindow):
+        MainWindow.setObjectName("Noya's Planets Detection")
+        MainWindow.resize(611, 505)
+        
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        
+        # Add a label with the text "Upload Planet"
+        self.titleLabel = QLabel("Upload Planet", self.centralwidget)
+        self.titleLabel.setAlignment(Qt.AlignCenter)
+        self.titleLabel.setGeometry(0, 0, 611, 50)
+        self.titleLabel.setObjectName("titleLabel")
+
+        # Customize the font of the label
+        font = self.titleLabel.font()
+        font.setFamily("Calibri Light")
+        font.setPointSize(24)
+        font.setBold(True)
+        self.titleLabel.setFont(font)
+
+        # Add a QLabel for displaying the uploaded photo
+        self.photoLabel = QLabel(self.centralwidget)
+        self.photoLabel.setGeometry(150, 100, 300, 300)
+        self.photoLabel.setObjectName("photoLabel")
+        self.photoLabel.setScaledContents(True)  # Ensure the image scales to fit the label
+        
+        # Add a button for browsing files
+        self.browseButton = QPushButton("Browse File", self.centralwidget)
+        self.browseButton.setGeometry(250, 420, 100, 30)
+        self.browseButton.setObjectName("browseButton")
+        self.browseButton.clicked.connect(self.browseFile)
+
+        # Add a button for prediction
+        self.predictButton = QPushButton("Predict", self.centralwidget)
+        self.predictButton.setGeometry(250, 460, 100, 30)
+        self.predictButton.setObjectName("predictButton")
+        self.predictButton.setVisible(False)  # Initially hidden
+        self.predictButton.clicked.connect(self.predictPhoto)
+        
+        # Add a label with the text "Upload Planet"
+        self.resultLabel = QLabel("Your Planet is:", self.centralwidget)
+        self.resultLabel.setAlignment(Qt.AlignCenter)  # Align text to center
+        self.resultLabel.setGeometry(0, 0, 611, 50)
+        self.resultLabel.setObjectName("resultLabel")
+        self.resultLabel.setVisible(False)
+
+        # Customize the font of the label
+        font = self.resultLabel.font()
+        font.setFamily("Calibri Light")
+        font.setPointSize(24)
+        font.setBold(True)
+        self.resultLabel.setFont(font)
+
+    def browseFile(self):
+        # Open a file dialog and get the selected file path
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly
+        fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", 
+                                                  "Image Files (*.png *.jpg *.jpeg *.bmp *.gif)", options=options)
+        if fileName:
+            # Display the selected photo
+            pixmap = QPixmap(fileName)
+            self.photoLabel.setPixmap(pixmap)
+            # Show the predict button
+            self.predictButton.setVisible(True)
+
+    def predictPhoto(self):
+        self.titleLabel.setVisible(False)
+        self.photoLabel.setVisible(False)
+        self.browseButton.setVisible(False)
+        self.predictButton.setVisible(False)
+
+        pixmap = self.photoLabel.pixmap()
+        if pixmap is not None:
+            image = pixmap.toImage()
+            input_data = self.preprocessImage(image)
+            
+            prediction = model.predict(input_data)
+            predicted_class = self.decodePrediction(prediction)
+
+            self.resultLabel.setText(f"Your planet is: {predicted_class}")
+            self.resultLabel.setVisible(True)
+
+    def preprocessImage(self, image):
+        image = image.scaled(256, 144, Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
+        width = image.width()
+        height = image.height()
+        ptr = image.bits()
+        ptr.setsize(image.byteCount())
+        arr = np.array(ptr).reshape(height, width, 4)
+        arr = arr[:, :, :3]
+        arr = arr / 255.0
+        arr = np.expand_dims(arr, axis=0)
+        return arr
+
+    def decodePrediction(self, prediction):
+        class_index = np.argmax(prediction, axis=1)[0]
+        class_names = ['Earth', 'Jupiter', 'MakeMake', 'Mars', 'Mercury', 'Moon', 'Neptune', 'Pluto', 'Saturn', 'Uranus', 'Venus']
+        return class_names[class_index]
+
+model = keras.models.load_model('../models/planet_detecion_model.keras')
+app = QApplication(sys.argv)
+MainWindow = QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUI(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
+
